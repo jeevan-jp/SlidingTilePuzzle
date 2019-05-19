c98f1c2be0e3fd0fc6427850739de16b9f195a2d
Jeevan <jiwanprakash65@gmail.com>
2019-05-19 14:15:32 +0530

    fix

--- src/components/ButtonBoard/ButtonBoard.js
+++ src/components/ButtonBoard/ButtonBoard.js
@@ -4,68 +4,72 @@ import styled from 'styled-components';
 import Button from '../Button';
 // import { sayHi } from '../../utils/misc';
 
-const ButtonBoard = (props) => {
-  const [row, setCurrentRow] = useState(4);
-  const [column, setCurrentColumn] = useState(4);
-  const [board, setBoard] = useState([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15, '']]);
-  
-  useEffect(() => {  
-    const handleLeft = () => {
-      if(column > 1) {
-        setCurrentColumn(column - 1);
-        console.log(row, column);
-      }
+class ButtonBoard extends React.Component {
+  state = {
+    row: 4,
+    column: 4,
+    board: [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15, '']],
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentDidUpdate() {
+    const { row,column } = this.state;
+    console.log(row, column);
+  }
+
+  handleLeft = () => {
+    const { column } = this.state;
+    if(column > 1) {
+      this.setState({ column: column - 1 });
     }
+  }
 
-    const handleTop = () => {
-      if(row > 1) {
-        setCurrentRow(row - 1);
-        console.log(row, column);
-      }
+  handleTop = () => {
+    const { row } = this.state;
+    if(row > 1) {
+      this.setState({ row: row - 1 });
     }
+  }
 
-    const handleRight = () => {
-      if(column < 4) {
-        setCurrentColumn(column + 1);
-        console.log(row, column);
-      }
+  handleRight = () => {
+    const { column } = this.state;
+    if(column < 4) {
+      this.setState({ column: column + 1 });
     }
+  }
 
-    const handleDown = () => {
-      if(row < 4) {
-        setCurrentRow(row + 1);
-        console.log(row, column);
-      }
+  handleDown = () => {
+    const { row, column } = this.state;
+    if(row < 4) {
+      this.setState({ row: row + 1 });
     }
+  }
 
-    const handleKeyDown = (e) => {
-      const { keyCode } = e;
-      switch(keyCode) {
-        case 37:
-          handleLeft();
-          break;
-        case 38:
-          handleTop();
-          break;
-        case 39:
-          handleRight();
-          break;
-        case 40:
-          handleDown();
-          break;
-        default:
-          break;
-      }
+  handleKeyDown = (e) => {
+    const { keyCode } = e;
+    switch(keyCode) {
+      case 37:
+        this.handleLeft();
+        break;
+      case 38:
+        this.handleTop();
+        break;
+      case 39:
+        this.handleRight();
+        break;
+      case 40:
+        this.handleDown();
+        break;
+      default:
+        break;
     }
+  }
 
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.addEventListener('keydown', handleKeyDown);
-    }
-  }, [row, column]);
-
-  const renderButtons = () => {
+  renderButtons = () => {
+    const { board } = this.state;
     return board.flat().map(num => (
       num === '' ? (
       <Button bgColor={"green"} key={'button' + num}>
@@ -79,13 +83,15 @@ const ButtonBoard = (props) => {
     ));
   }
 
-  return (
-    <Container>
-      <ButtonContainer>
-        {renderButtons()}
-      </ButtonContainer>
-    </Container>
-  );
+  render() {
+    return (
+      <Container>
+        <ButtonContainer>
+          {this.renderButtons()}
+        </ButtonContainer>
+      </Container>
+    );
+  }
 }
 
 export default ButtonBoard;
